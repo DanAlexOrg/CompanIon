ba1e0d0cbe7bb6286518db14844f5cb78e6d33a4
Aleksei Danilushkin <dan-aleksei2012@yandex.ru>
2022-05-21 13:55:24 +0300

    T-001 Add main class

---
+++ companion-file/src/main/java/org/danalex/companionfile/CompanionFileApplication.java
@@ -0,0 +1,17 @@
+package org.danalex.companionfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>Service for downloading and uploading of files.</p>
+ */
+@SpringBootApplication
+public class CompanionFileApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CompanionFileApplication.class, args);
+	}
+
+}
