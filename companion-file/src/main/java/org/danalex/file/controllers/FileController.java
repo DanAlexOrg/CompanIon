c2b9a43b21a161819e875e5abb3c5a46d91819fc
Aleksei Danilushkin <dan-aleksei2012@yandex.ru>
2022-05-29 14:34:29 +0300

    T-001 Add get method into file controller

--- companion-file/src/main/java/org/danalex/file/controllers/FileController.java
+++ companion-file/src/main/java/org/danalex/file/controllers/FileController.java
@@ -1,9 +1,12 @@
 package org.danalex.file.controllers;
 
 import lombok.AllArgsConstructor;
+import org.danalex.file.dto.DownloadFileDTO;
 import org.danalex.file.services.FileService;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
 
 /**
  * <p>Controller for handling upload and download of files requests.</p>
@@ -15,13 +18,28 @@ public class FileController {
     private final FileService fileService;
 
     /**
+     * <p>Handles a request to download a file.</p>
+     *
+     * @param id ID of file.
+     *
+     * @return File download data.
+     */
+    @GetMapping("/api/v1/download/{id}")
+    public @ResponseBody DownloadFileDTO download(@PathVariable UUID id) {
+
+        return DownloadFileDTO.of(this.fileService.get(id));
+    }
+
+    /**
      * <p>Handles a request to upload a file.</p>
      *
      * @param file File.
+     *
+     * @return File download data.
      */
     @PostMapping("/api/v1/upload")
-    public void upload(@RequestParam("file") MultipartFile file)  {
+    public @ResponseBody DownloadFileDTO upload(@RequestParam("file") MultipartFile file)  {
 
-        this.fileService.save(file);
+        return DownloadFileDTO.of(this.fileService.save(file));
     }
 }
