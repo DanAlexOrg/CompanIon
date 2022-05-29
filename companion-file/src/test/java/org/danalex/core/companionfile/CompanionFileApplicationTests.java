5578407b55aead5cd2a66d259b921ac5ae5bfa73
Aleksei Danilushkin <dan-aleksei2012@yandex.ru>
2022-05-29 12:43:22 +0300

    T-001 Add test class

---
+++ companion-file/src/test/java/org/danalex/core/companionfile/CompanionFileApplicationTests.java
@@ -0,0 +1,13 @@
+package org.danalex.core.companionfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanionFileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
